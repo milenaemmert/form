ac240bde62c9f8ab03bfc1b66369dfc50a42a6f6
Milena Emmert <milena.emmert@outlook.com>
2022-07-16 16:40:04 -0300

    Replaces a full water bottle image by an empty bottle and checks the amount of water consumed

---
+++ assets/js/water.js
@@ -0,0 +1,32 @@
+const bottlesNodeList = document.querySelectorAll('.water')
+const bottles = Object.values(bottlesNodeList)
+let amountOfWater = 0
+const arrayTotalWater = []
+
+bottles.forEach(elem => {
+    elem.addEventListener('click', () => {
+        let drankWater
+        if(elem.src == 'http://127.0.0.1:5501/assets/imgs/bottle.png') {
+            elem.setAttribute('src', 'assets/imgs/empty-bottle.png')
+            drankWater = true
+            const amountOfWater = trackWater(drankWater)
+            setWaterConsumed(amountOfWater)
+        } else {
+            elem.setAttribute('src', 'assets/imgs/bottle.png')
+            drankWater = false
+            const amountOfWater = trackWater(drankWater)
+            setWaterConsumed(amountOfWater)
+        }
+    })
+})
+
+function trackWater(arg) {
+    arg == true ? arrayTotalWater.push(500) : arrayTotalWater.push(-500)
+    const amountOfWater = calculateWater(arrayTotalWater)
+    return amountOfWater
+}
+
+function calculateWater(arrayTotalWater) {
+    amountOfWater = arrayTotalWater.reduce((ml, i) => { return ml + i })
+    return amountOfWater
+}
